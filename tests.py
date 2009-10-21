23d5dd5be620bee519d97c651f8da80179936d72
Chris Drackett <chris@chrisdrackett.com>
2009-10-21 16:53:09 -0500

    formatting.

--- tests.py
+++ tests.py
@@ -11,11 +11,11 @@ class TestSubscription(TestCase):
     def setUp(self):
         user = User.objects.create(username='test')
         self.sclient = Client(settings.SPREEDLY_AUTH_TOKEN, settings.SPREEDLY_SITE_NAME)
-
+    
     def tearDown(self):
         # Remove all subscribers
         self.sclient.cleanup()
-
+    
     def test_sync_plans(self):
         # Initial sync
         spreedly_count = len(self.sclient.get_plans())
