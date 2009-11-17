9734965fe7aeed0c0bb720629c3dd043524f3652
Chris Drackett <chris@chrisdrackett.com>
2009-11-17 01:07:04 -0600

    adding quick sub. admin.

--- admin.py
+++ admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from spreedly.models import Gift
+from spreedly.models import Gift, Subscription
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'lifetime', 'active_until', 'active')
 
 admin.site.register(Gift)
+admin.site.register(Subscription, SubscriptionAdmin)
